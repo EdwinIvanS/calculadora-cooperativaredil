76488b152eb8079ac4b9303c8790f0470e1d0ae6
EdwinIvanS <sedwin118@hotmail.com>
2023-03-02 15:16:46 -0500

    cuerpo del proyecto

---
+++ public/utils/LineaCredito.js
@@ -0,0 +1,19 @@
+const linea = {
+  vacio: "",
+  libreinversion: "Libre inversion",
+  rediplus: "RediPlus",
+  anticipo: "Anticipo de Prima",
+  tarjeta: "Tarjeta Integral",
+  hogarA: "Hogar y Alianzas",
+  salud: "Salud",
+  emergencia: "Emergencia",
+  educacion: "Educación",
+  restauracion: "Restauracion Financiera",
+  pensionados: "Pensionados",
+  vehiculo: "Vehículo",
+  capitalT: "Capital de Trabajo",
+};
+
+;
+
+module.exports = linea;
