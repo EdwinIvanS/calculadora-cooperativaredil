567d4bb776ea9e0c7418e6dd44f37ec08375201e
EdwinIvanS <sedwin118@hotmail.com>
2023-03-02 16:11:20 -0500

    creando objeto linea para consumo de los select

--- public/utils/LineaCredito.js
+++ public/utils/LineaCredito.js
@@ -1,59 +1,163 @@
 const linea = [
   {
-    id: "0",
-    value: "",
+    id: "vacio",
+    value: " - ",
   },
   {
     id: "linv",
     value: "Libre inversion",
+    plazo: [
+      {
+        value: " - ",
+        value: "12",
+        value: "24",
+        value: "36",
+        value: "48",
+        value: "60",
+      },
+    ],
   },
   {
     id: "rPlus",
     value: "RediPlus",
+    plazo: [
+      {
+        value: " - ",
+        value: "12",
+        value: "24",
+        value: "36",
+        value: "48",
+        value: "60",
+      },
+    ],
   },
   {
     id: "unOb",
     value: "Unificación de Obligaciones",
+    plazo: [
+      {
+        value: " - ",
+        value: "12",
+        value: "24",
+        value: "36",
+        value: "48",
+        value: "60",
+      },
+    ],
   },
   {
     id: "aPri",
-    value: "Anticipo de Prima",
+    value: "Anticipo de Prima", //revisar
   },
   {
     id: "tInt",
     value: "Tarjeta Integral",
+    plazo: [
+      {
+        value: " - ",
+        value: "24",
+      },
+    ],
   },
   {
     id: "HyA",
     value: "Hogar y Alianzas",
+    uso: [
+      {
+        value: " - ",
+        value: "Remodelación",
+        value: "Turismo",
+      },
+    ],
   },
   {
     id: "Salud",
     value: "Salud",
+    plazo: [
+      {
+        value: " - ",
+        value: "12",
+        value: "24",
+        value: "36",
+        value: "48",
+        value: "60",
+      },
+    ],
   },
   {
     id: "Emrg",
     value: "Emergencia",
+    plazo: [
+      {
+        value: " - ",
+        value: "12",
+        value: "24",
+        value: "36",
+        value: "48",
+        value: "60",
+      },
+    ],
   },
   {
     id: "Edu",
     value: "Educación",
+    uso: [
+      {
+        value: " - ",
+        value: "Semestre",
+        value: "Especialización",
+      },
+    ],
   },
   {
     id: "rFin",
     value: "Restauracion Financiera",
+    plazo: [
+      {
+        value: " - ",
+        value: "60",
+      },
+    ],
   },
   {
     id: "Pen",
     value: "Pensionados",
+    plazo: [
+      {
+        value: " - ",
+        value: "12",
+        value: "24",
+        value: "36",
+        value: "48",
+        value: "60",
+        value: "72",
+      },
+    ],
   },
   {
     id: "veh",
     value: "Vehículo",
+    uso: [
+      {
+        value: " - ",
+        value: "Nuevo",
+        value: "Usado",
+      },
+    ],
   },
   {
     id: "cTrabajo",
     value: "Capital de Trabajo",
+    plazo: [
+      {
+        value: " - ",
+        value: "12",
+        value: "24",
+        value: "36",
+        value: "48",
+        value: "60"
+      },
+    ]
   },
 ];
 
