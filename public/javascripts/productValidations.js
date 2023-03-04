a163238487c72dcc41bdf314243d5c215a07d957
EdwinIvanS <sedwin118@hotmail.com>
2023-03-03 22:16:35 -0500

    agregando validacion para el plazo

--- public/javascripts/productValidations.js
+++ public/javascripts/productValidations.js
@@ -1,22 +1,25 @@
+
 window.addEventListener("load", function () {
   let selectProduct = document.querySelector("#linea");
   let monto = document.querySelector("#visible");
   let plazo = document.querySelector("#plazo");
   let btn = document.querySelector("#btn");
 
+  //document.querySelector("#h2").innerHTML = e.target.value;
+
   selectProduct.onchange = (e) => {
-    
+
     if (e.target.value != "vacio" || e.target.value != "") {
       document.querySelector(".oculto").style.display = "block";
-      document.querySelector(".oculto").focus();  
-      //document.querySelector("#h2").innerHTML = "bloque if " + e.target.value;    
+      document.querySelector(".oculto").focus();
+      //document.querySelector("#h2").innerHTML = e.target.value;
+      document.getElementById("prueba").value = e.target.value;
+
     }
 
     if (e.target.value == "vacio" || e.target.value == "") {
       document.querySelector(".oculto").style.display = "none";
     }
-      
-    
   };
 
   monto.onchange = (e) => {
--- public/utils/LineaCredito.js
+++ public/utils/LineaCredito.js
@@ -2,162 +2,129 @@ const linea = [
   {
     id: "vacio",
     value: " - ",
+    plazo: [],
   },
   {
     id: "linv",
     value: "Libre inversion",
     plazo: [
-      {
-        value: " - ",
-        value: "12",
-        value: "24",
-        value: "36",
-        value: "48",
-        value: "60",
-      },
+      { value: " - " },
+      { value: "12" },
+      { value: "24" },
+      { value: "36" },
+      { value: "48" },
+      { value: "60" },
     ],
   },
   {
     id: "rPlus",
     value: "RediPlus",
     plazo: [
-      {
-        value: " - ",
-        value: "12",
-        value: "24",
-        value: "36",
-        value: "48",
-        value: "60",
-      },
+      { value: " - " },
+      { value: "12" },
+      { value: "24" },
+      { value: "36" },
+      { value: "48" },
+      { value: "60" },
     ],
   },
   {
     id: "unOb",
     value: "Unificación de Obligaciones",
     plazo: [
-      {
-        value: " - ",
-        value: "12",
-        value: "24",
-        value: "36",
-        value: "48",
-        value: "60",
-      },
+      { value: " - " },
+      { value: "12" },
+      { value: "24" },
+      { value: "36" },
+      { value: "48" },
+      { value: "60" },
     ],
   },
   {
     id: "aPri",
     value: "Anticipo de Prima", //revisar
+    plazo: [],
   },
   {
     id: "tInt",
     value: "Tarjeta Integral",
-    plazo: [
-      {
-        value: " - ",
-        value: "24",
-      },
-    ],
+    plazo: [{ value: " - " }, { value: "24" }],
   },
   {
     id: "HyA",
     value: "Hogar y Alianzas",
-    uso: [
-      {
-        value: " - ",
-        value: "Remodelación",
-        value: "Turismo",
-      },
-    ],
+    plazo: [],
+    uso: [{ value: " - " }, { value: "Remodelación" }, { value: "Turismo" }],
   },
   {
     id: "Salud",
     value: "Salud",
     plazo: [
-      {
-        value: " - ",
-        value: "12",
-        value: "24",
-        value: "36",
-        value: "48",
-        value: "60",
-      },
+      { value: " - " },
+      { value: "12" },
+      { value: "24" },
+      { value: "36" },
+      { value: "48" },
+      { value: "60" },
     ],
   },
   {
     id: "Emrg",
     value: "Emergencia",
     plazo: [
-      {
-        value: " - ",
-        value: "12",
-        value: "24",
-        value: "36",
-        value: "48",
-        value: "60",
-      },
+      { value: " - " },
+      { value: "12" },
+      { value: "24" },
+      { value: "36" },
+      { value: "48" },
+      { value: "60" },
     ],
   },
   {
     id: "Edu",
     value: "Educación",
+    plazo: [],
     uso: [
-      {
-        value: " - ",
-        value: "Semestre",
-        value: "Especialización",
-      },
+      { value: " - " },
+      { value: "Semestre" },
+      { value: "Especialización" },
     ],
   },
   {
     id: "rFin",
     value: "Restauracion Financiera",
-    plazo: [
-      {
-        value: " - ",
-        value: "60",
-      },
-    ],
+    plazo: [{ value: " - " }, { value: "60" }],
   },
   {
     id: "Pen",
     value: "Pensionados",
     plazo: [
-      {
-        value: " - ",
-        value: "12",
-        value: "24",
-        value: "36",
-        value: "48",
-        value: "60",
-        value: "72",
-      },
+      { value: " - " },
+      { value: "12" },
+      { value: "24" },
+      { value: "36" },
+      { value: "48" },
+      { value: "60" },
+      { value: "72" },
     ],
   },
   {
     id: "veh",
     value: "Vehículo",
-    uso: [
-      {
-        value: " - ",
-        value: "Nuevo",
-        value: "Usado",
-      },
-    ],
+    plazo: [],
+    uso: [{ value: " - " }, { value: "Nuevo" }, { value: "Usado" }],
   },
   {
     id: "cTrabajo",
     value: "Capital de Trabajo",
     plazo: [
-      {
-        value: " - ",
-        value: "12",
-        value: "24",
-        value: "36",
-        value: "48",
-        value: "60"
-      },
-    ]
+      { value: " - " },
+      { value: "12" },
+      { value: "24" },
+      { value: "36" },
+      { value: "48" },
+      { value: "60" },
+    ],
   },
 ];
 
