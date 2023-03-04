7f1855dc76482bc76b600db59a111bfc1e0c3481
EdwinIvanS <sedwin118@hotmail.com>
2023-03-04 17:40:32 -0500

    agregando options para select de plazo

--- public/javascripts/productValidations.js
+++ public/javascripts/productValidations.js
@@ -1,39 +1,68 @@
 
 window.addEventListener("load", function () {
   let selectProduct = document.querySelector("#linea");
+  let plazos = document.querySelector("#plazoS");
   let monto = document.querySelector("#visible");
   let plazo = document.querySelector("#plazo");
   let btn = document.querySelector("#btn");
+  let option0 = document.createElement("option");
+  let option1 = document.createElement("option");
+  let option2 = document.createElement("option");
+  let option3 = document.createElement("option");
+  let option4 = document.createElement("option");
+  let option5 = document.createElement("option");
+  let option6 = document.createElement("option");
 
   //document.querySelector("#h2").innerHTML = e.target.value;
 
   selectProduct.onchange = (e) => {
-
     if (e.target.value != "vacio" || e.target.value != "") {
       document.querySelector(".oculto").style.display = "block";
-      document.querySelector(".oculto").focus();
-      //document.querySelector("#h2").innerHTML = e.target.value;
-      document.getElementById("prueba").value = e.target.value;
-
+      document.querySelector("#h2").innerHTML = e.target.value;
     }
-
     if (e.target.value == "vacio" || e.target.value == "") {
       document.querySelector(".oculto").style.display = "none";
     }
+
+    if (
+      e.target.value == "linv" || e.target.value == "rPlus" ||
+      e.target.value == "unOb" || e.target.value == "Salud" ||
+      e.target.value == "Emrg" || e.target.value == "cTrabajo"
+    ) {
+      option0.value = " - ", option0.text = "-", plazos.add(option0);
+      option1.value = "12", option1.text = "12", plazos.add(option1);
+      option2.value = "24", option2.text = "24", plazos.add(option2);
+      option3.value = "36", option3.text = "36", plazos.add(option3);
+      option4.value = "48", option4.text = "48", plazos.add(option4);
+      option5.value = "60", option5.text = "60", plazos.add(option5);
+      plazos.remove(option6);
+    }
+
+    if (
+      e.target.value == "Pen"
+    ) {
+      (option0.value = " - "), (option0.text = "-"), plazos.add(option0);
+      (option1.value = "12"), (option1.text = "12"), plazos.add(option1);
+      (option2.value = "24"), (option2.text = "24"), plazos.add(option2);
+      (option3.value = "36"), (option3.text = "36"), plazos.add(option3);
+      (option4.value = "48"), (option4.text = "48"), plazos.add(option4);
+      (option5.value = "60"), (option5.text = "60"), plazos.add(option5);
+      (option6.value = "72"), (option6.text = "72"), plazos.add(option6);
+    }
+
+    
   };
 
   monto.onchange = (e) => {
     if (e.target.value != ".oculto") {
       document.querySelector("#plazo").style.display = "block";
-      document.querySelector("#plazo").focus();
     }
   };
 
   plazo.onchange = (e) => {
-    if (e.target.value != ".oculto") {
+    if (e.target.value != ".oculto") {      
       document.querySelector("#btn").style.display = "table-row-group";
-      document.querySelector("#btn").focus();
-    }
+    }        
   };
 
   btn.onclick = (e) => {
